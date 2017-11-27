8ba64e42d49578b1de77aba68862150a0c41c51e
Stephen McCann <info@flatsteve.com>
2017-11-27 21:54:44 +0000

    Get task ready

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+// Endpoint to get arrivals https://pair-programming-task.firebaseio.com/arrivals.json (accepts GET & PATCH)
+// Returns denormalised data - object with keys (not array)
+// API Follows standard REST patterns e.g. updating an arrival `arrivals/${arrivalKey}.json`
 
 class App extends Component {
-  // Endpoint to get arrivals https://pair-programming-task.firebaseio.com/arrivals.json (accepts GET & PATCH)
-  // Follows standard REST
-  
-  
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Airsorted Arrivals</h1>
+          <h1>Airsorted Arrivals</h1>
         </header>
-        <p className="App-intro">
-          {/* TODO SHOW ARRIVALS */}
-        </p>
+
+        <div className="App-content">
+          {/* TODO SHOW ARRIVALS - ASSUME ENDPOINT ONLY RETURNS TODAYS ARRIVALS */}
+          {/* TODO MAKE LOOK DECENT :) */}
+          {/* TODO ORDER BY TIME */}
+          {/* TODO ALLOW ARRIVAL TO BE UPDATED TO "ARRIVED" CHANGE COLOR */}
+        </div>
       </div>
     );
   }
