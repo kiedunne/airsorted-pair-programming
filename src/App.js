d1e7d6052b55e525feba11ffd81d56a09ca48552
Stephen McCann <info@flatsteve.com>
2017-11-28 10:26:40 +0000

    Add readme

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import "./App.css";
 
 // Endpoint to get arrivals https://pair-programming-task.firebaseio.com/arrivals.json (accepts GET & PATCH)
 // Returns denormalised data - object with keys (not array)
-// API Follows standard REST patterns e.g. updating an arrival `arrivals/${arrivalKey}.json`
+// API Follows standard REST patterns e.g. updating an arrival `arrivals/${arrivalKey}.json` - body: { thing_that_changed: value}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Airsorted Arrivals</h1>
+          <h1>Airsorted Guest Arrivals</h1>
         </header>
 
         <div className="App-content">
           {/* TODO SHOW ARRIVALS - ASSUME ENDPOINT ONLY RETURNS TODAYS ARRIVALS */}
           {/* TODO MAKE LOOK DECENT :) */}
           {/* TODO ORDER BY TIME */}
-          {/* TODO ALLOW ARRIVAL TO BE UPDATED TO "ARRIVED" CHANGE COLOR */}
+          {/* TODO ALLOW ARRIVAL TO BE UPDATED TO "ARRIVED" & CHANGE COLOR */}
         </div>
       </div>
     );
