b66ccbb6ae9919b10c9e74aa42ce4e574e73a4eb
Stephen McCann <info@flatsteve.com>
2018-01-22 11:31:41 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 
 // Endpoint to get arrivals https://pair-programming-task.firebaseio.com/arrivals.json (accepts GET & PATCH)
-// Returns denormalised data - object with keys (not array)
+// Returns array of arrivals for today
 // API Follows standard REST patterns e.g. updating an arrival `arrivals/${arrivalKey}.json` - body: { thing_that_changed: value}
 
 class App extends Component {
@@ -15,7 +15,6 @@ class App extends Component {
 
         <div className="App-content">
           {/* TODO SHOW ARRIVALS - ASSUME ENDPOINT ONLY RETURNS TODAYS ARRIVALS */}
-          {/* TODO MAKE LOOK DECENT :) */}
           {/* TODO ORDER BY TIME */}
           {/* TODO ALLOW ARRIVAL TO BE UPDATED TO "ARRIVED" & CHANGE COLOR */}
         </div>
