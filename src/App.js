dceef2e451a07b72de25dc993a9fdde1d5331fb6
Stephen McCann <info@flatsteve.com>
2018-04-19 16:19:48 +0100

    Remove solution

--- src/App.js
+++ src/App.js
@@ -7,45 +7,6 @@ import "./App.css";
 // API Follows standard REST patterns e.g. updating a booking `/bookings/${id}` - body: { thing_that_changed: value}
 
 class App extends Component {
-  state = {
-    bookings: []
-  };
-
-  getBookings() {
-    fetch("http://localhost:8000/bookings")
-      .then(res => {
-        return res.json();
-      })
-      .then(bookings => {
-        this.setState({
-          bookings
-        });
-      });
-  }
-
-  updateBooking(booking) {
-    fetch(`http://localhost:8000/bookings/${booking._id}`, {
-      method: "PATCH",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        cancelled: !booking.cancelled
-      })
-    })
-      .then(res => {
-        return res.json();
-      })
-      .then(booking => {
-        this.getBookings();
-      });
-  }
-
-  componentDidMount() {
-    this.getBookings();
-  }
-
   render() {
     return (
       <div className="app">
@@ -59,23 +20,7 @@ class App extends Component {
           {/* TODO ALLOW BOOKING TO BE CANCELLED/ACTIVATED - CHANGE STYLE TO MAKE APPARENT */}
           {/* WRITE A TEST TO ENSURE LIST OF BOOKINGS IS RENDERED */}
 
-          {!this.state.bookings.length ? (
-            <p className="empty-alert">No bookings found...</p>
-          ) : (
-            this.state.bookings.map(booking => {
-              return (
-                <div key={booking._id}>
-                  <p>{booking.name}</p>
-                  <p>{booking.cancelled ? "Inaction" : "Active"}</p>
-                  <p>{booking.date.toLocaleString()}</p>
-
-                  <button onClick={() => this.updateBooking(booking)}>
-                    Update
-                  </button>
-                </div>
-              );
-            })
-          )}
+          <p className="empty-alert">No bookings found...</p>
         </div>
       </div>
     );
