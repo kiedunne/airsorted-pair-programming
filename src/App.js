d8028e41294601b0570cf568cc6b8b00f82927e4
Stephen McCann <info@flatsteve.com>
2018-04-20 16:59:49 +0100

    Remove solution

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import axios from "axios";
-import Bookings from "./Bookings";
 import "./App.css";
 
 // Bookings endpoint http://localhost:8000/bookings (accepts GET & PATCH only)
@@ -8,62 +6,20 @@ import "./App.css";
 // PATCHING a booking will return the single resource that changed
 // API Follows standard REST patterns e.g. updating a booking `/bookings/${id}` - body: { thing_that_changed: value}
 
-// TODO SHOW LIST OF BOOKINGS 
-// TODO ORDER BOOKINGS BY ARRIVAL TIME SERVER OR CLIENT - UP TO YOU 
-// TODO ALLOW BOOKING TO BE CANCELLED/ACTIVATED - CHANGE STYLE TO MAKE APPARENT 
+// TODO SHOW LIST OF BOOKINGS
+// TODO ORDER BOOKINGS BY ARRIVAL TIME SERVER OR CLIENT - UP TO YOU
+// TODO ALLOW BOOKING TO BE CANCELLED/ACTIVATED - CHANGE STYLE TO MAKE APPARENT
 // TODO WRITE A TEST TO ENSURE LIST OF BOOKINGS IS RENDERED
 
 class App extends Component {
-  state = {
-    bookings: []
-  };
-
-  getBookings() {
-    axios.get("http://localhost:8000/bookings").then(res => {
-      this.setState({
-        bookings: res.data.sort((a, b) => {
-          return a.date > b.date ? 1 : -1;
-        })
-      });
-    });
-  }
-
-  componentDidMount() {
-    this.getBookings();
-  }
-
-  toggleBooking = booking => {
-    axios
-      .patch(`http://localhost:8000/bookings/${booking._id}`, {
-        cancelled: !booking.cancelled
-      })
-      .then(res => {
-        this.setState({
-          bookings: this.state.bookings.map(booking => {
-            return booking._id === res.data._id
-              ? { ...booking, cancelled: res.data.cancelled }
-              : booking;
-          })
-        });
-      });
-  };
-
   render() {
-    const { bookings } = this.state;
-
     return (
       <div className="app">
         <header className="app-header">
           <h1>Airsorted Bookings Dashboard</h1>
         </header>
 
-        <div className="app-content">
-          {bookings.length ? (
-            <Bookings bookings={bookings} toggleBooking={this.toggleBooking} />
-          ) : (
-            <p className="empty-alert">No bookings found...</p>
-          )}
-        </div>
+        <p className="empty-alert">No bookings found...</p>
       </div>
     );
   }
